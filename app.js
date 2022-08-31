26e76c8d9b0ba2694cdfe472f799cc5c1b1de1bd
martin berna <bernacheamartin@outlook.com>
2022-08-31 11:34:08 -0300

    Agrego el BUSCALO POR MARCA + Imagenes. Modifico app.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const path = require('path');
+// Se cargan los modulos necesarios.
+var express = require('express');
+var path = require('path');
 
-const app = express();
+// Crea una Express app.
+var app = express();
 
-app.get('/', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
+// obtiene la ruta del directorio publico donde se encuentran los elementos estaticos (css, js).
+var publicPath = path.resolve(__dirname, '/public'); //path.join(__dirname, 'public'); también puede ser una opción
+
+// Para que los archivos estaticos queden disponibles.
+app.use(express.static(publicPath));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-app.use(express.static(path.resolve(__dirname, './public')));
-
+app.get('/css', function (req, res) {
+    res.sendFile(__dirname + '/public/assets/css/stylesheet.css');
+});
 
 app.listen(3002, () => {
     console.log("Servidor corriendo");
