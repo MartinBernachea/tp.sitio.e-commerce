b1e119019fddc8fc552bd72ca6687e91572053e9
martin berna <bernacheamartin@outlook.com>
2022-09-05 19:14:25 -0300

    Agrego coming-soon.html + CSS, respectivos href

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/coming-soon', function (req, res) {
+    res.sendFile(__dirname + '/views/pages/coming-soon.html');
+});
+
 app.get('/css', function (req, res) {
     res.sendFile(__dirname + '/public/assets/css/stylesheet.css');
 });
