48e03f617d293f22b16f5e0c4e1243fbc2d43483
TobiGondolo <tobiasgondolo@gmail.com>
2022-10-04 21:01:22 -0300

    fix index.ejs

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 const controller = {
     index: (req, res) => {
-        res.render('index');
+        res.render('index',{productos:products});
     },
     products: (req, res) => {
         res.render('./pages/detalleProducto')
