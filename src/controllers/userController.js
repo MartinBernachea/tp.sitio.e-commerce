454dcce9a6cfff026f13597ab26d9141976ef89a
TobiGondolo <tobiasgondolo@gmail.com>
2022-11-07 13:13:33 -0300

    add hash

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,45 @@
+// ************ Require's ************
+const bcrypt = require ('bcrypt');
+const path = require ('path');
+const fs = require ('fs');
+const multer = require ('multer');
+
+const userFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const user = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
+
+
 const controller = {
     login: (req, res) => {
         res.render('./pages/formLogin');
     },
     register: (req, res) => {
         res.render('./pages/formRegister');
+    },
+    userStore: (req, res, next) => {
+        let nuevoUsuario = {
+            nombre: req.body.cName,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10)
+        }
+
+        user.push(nuevoUsuario);
+
+        fs.writeFileSync(productsFilePath, JSON.stringify(user, null, ' '), 'utf-8');
+
+        res.redirect('/');
+
+    },
+    processLogin: (req, res) => {
+        let archivoUsuario = fs.readFileSync('usersDataBase.json', {encoding: 'utf-8'}); 
+        let usuarios;
+        if (archivoUsuario == ""){
+            usuarios = [];
+        } else {
+            usuarios = JSON.parse(archivoUsuario);
+        }
+        for (let i=0; i<= usuarios.length; i++){
+            if(req.body.email == usuarios [i].email && bcrypt.compareSync(req.body.password, usuarios[i].password));
+        }
     }
 };
 module.exports = controller;
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,28 @@ const userController = require('./../controllers/userController');
 
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
+const path = require('path');
+const multer = require('multer');
+
+//***  MULTER  ****/
+
+const multerDiskStorage = multer.diskStorage({
+    destination: function(req, file, cb) {       // request, archivo y callback que almacena archivo en destino
+        cb(null, path.join(__dirname,'../../public/img'));    // Ruta donde almacenamos el archivo
+    },
+    filename: function(req, file, cb) {          // request, archivo y callback que almacena archivo en destino
+    let imageName = Date.now() + path.extname(file.originalname);   // milisegundos y extensión de archivo original
+        cb(null, imageName);         
+    }
+});
+
+const uploadFile = multer({ storage: multerDiskStorage });
 
 router.get('/register', userController.register);
-router.get('/login', userController.login)
+router.post('/register', userController.userStore); //uploadFile.single('uImage')
+
+router.get('/login', userController.login);
+router.post('/login', userController.processLogin);
 
 module.exports = router;
