8548db99117a38d7d0ee60dbaeea9264b965d8f4
martin berna <bernacheamartin@outlook.com>
2022-11-08 00:05:09 -0300

    more fix sprint5

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,12 @@
 // ************ Require's ************
-const bcrypt = require ('bcrypt');
-const path = require ('path');
-const fs = require ('fs');
-const multer = require ('multer');
+const bcrypt = require('bcrypt');
+const path = require('path');
+const fs = require('fs');
+const multer = require('multer');
 const session = require('express-session');
-const { validationResult } = require ('express-validator');
+const {
+    validationResult
+} = require('express-validator');
 
 
 const userFilePath = path.join(__dirname, '../data/usersDataBase.json');
@@ -13,7 +15,9 @@ const user = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
 
 const controller = {
     login: (req, res) => {
-        res.render('./pages/formLogin', {user: user});
+        res.render('./pages/formLogin', {
+            user: user
+        });
     },
     register: (req, res) => {
         res.render('./pages/formRegister');
@@ -22,48 +26,67 @@ const controller = {
         let errors = validationResult(req);
 
         let idNuevoUsuario = (user[user.length - 1].id) + 1;
-        
-        if(errors.isEmpty()){
-        let nuevoUsuario = {
-            "id": idNuevoUsuario,
-            "nombre": req.body.cName,
-            "email": req.body.email,
-            "password": bcrypt.hashSync(req.body.password, 10)
-            // "image": 
+
+        if (errors.isEmpty()) {
+            let nuevoUsuario = {
+                "id": idNuevoUsuario,
+                "nombre": req.body.cName,
+                "email": req.body.email,
+                "password": bcrypt.hashSync(req.body.password, 10)
+                // "image": 
+            }
+            user.push(nuevoUsuario);
+
+            fs.writeFileSync(userFilePath, JSON.stringify(user, null, ' '), 'utf-8');
+
+            res.redirect('/');
+        } else {
+            res.render('./pages/formRegister', {
+                errors: errors.array()
+            });
         }
-        user.push(nuevoUsuario);
 
-        fs.writeFileSync(userFilePath, JSON.stringify(user, null, ' '), 'utf-8');
-
-        res.redirect('/');
-    } else {
-        res.render('./pages/formRegister', {errors: errors.array() } );
-    }
-        
 
     },
     processLogin: (req, res) => {
         let errors = validationResult(req);
-        let archivoUsuario = fs.readFileSync(userFilePath, {encoding: 'utf-8'}); 
-        let usuarios;
+        let archivoUsuario = fs.readFileSync(userFilePath, {
+            encoding: 'utf-8'
+        });
+        let usuarios = JSON.parse(archivoUsuario);
+        let usuarioALoguearse;
 
-        if (errors.isEmpty()){
+        if (errors.isEmpty()) {
 
-            if (archivoUsuario == " "){
-                usuarios = [];
-                
-            } else {
-                usuarios = JSON.parse(archivoUsuario);
-                
-                for (let i=0; i<= usuarios.length; i++){
-                    if(req.body.email == usuarios[i].email && bcrypt.compareSync(req.body.password, usuarios[i].password));
-                    
-                    res.redirect('/');
-                    
+            for (let i = 0; i < usuarios.length; i++) {
+
+                if (usuarios[i].email == req.body.email) {
+                    usuarioALoguearse = usuarios[i];
                 }
             }
+
+            if (usuarioALoguearse != undefined) {
+
+                // let validPass = bcrypt.compareSync(req.body.password, usuarioALoguearse.password);
+                console.log(usuarioALoguearse.password);
+                console.log(req.body.password);
+                // console.log(validPass);
+
+                if (bcrypt.compareSync(req.body.password, usuarioALoguearse.password) == true) {
+
+                    res.redirect('/');
+
+                } else {
+                    res.send('password incorrecto');
+                }
+            } else {
+                res.send('email invalido')
+            }
+
         } else {
-            res.render('./pages/formLogin', {errors: errors.array() } ); 
+            res.render('./pages/formLogin', {
+                errors: errors.array()
+            });
         }
     }
 };
