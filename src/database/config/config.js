68892f17ea08178214c552378342bf5ec12702ca
TobiGondolo <tobiasgondolo@gmail.com>
2022-11-16 23:24:54 -0300

    config sequelize

---
+++ src/database/config/config.js
@@ -0,0 +1,25 @@
+module.exports = 
+  {
+    "development": {
+      "username": "root",
+      "password": null,
+      "database": "proyecto_final",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
+      "port": 3306
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "database_test",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "production": {
+      "username": "root",
+      "password": null,
+      "database": "database_production",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    }
+  }
