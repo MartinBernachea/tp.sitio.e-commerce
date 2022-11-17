7effe90697aade1069376d62b306fb82be1401bb
arsitta <sitta93@gmail.com>
2022-11-16 23:43:57 -0300

    ADD: Modelo tabla Usuario

---
+++ src/database/models/Usuarios.js
@@ -0,0 +1,38 @@
+function usuariosData(sequelize, Datatypes) {
+    let a = "Usuarios";
+
+    let b = {
+        id: {
+            type: Datatypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nombre: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
+        apellido: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
+        contra: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
+    }
+
+    // TODO: DEFINIR RELACION CON Local_id
+
+    let c = {
+        camelCase: false,
+        timestamps: false,
+    }
+
+    return sequelize.define(a, b, c);
+}
+
+module.exports = usuariosData
