1b9b69a6b5bfd401e06f09f08040f2f418769715
martin berna <bernacheamartin@outlook.com>
2022-12-07 22:54:07 -0300

    add upload img preview

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+// IMAGE UPLOAD
+
+function previewBeforeUpload(id) {
+    document.querySelector("#" + id).addEventListener("change", function (e) {
+        if (e.target.files.lenght == 0) {
+            return;
+        }
+        let file = e.target.files[0];
+        let url = URL.createObjectURL(file);
+        document.querySelector("#" + id + "-preview div").innerText = file.name;
+        document.querySelector("#" + id + "-preview img").src = url;
+    })
+}
+
+previewBeforeUpload("cImage1");
+previewBeforeUpload("cImage2");
+previewBeforeUpload("cImage3");
+previewBeforeUpload("cImage4");
+previewBeforeUpload("cImage5");
