ada5e60e7680abd57735a63a73b973971d4a92c3
martin berna <bernacheamartin@outlook.com>
2022-12-27 19:53:03 -0300

    update

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,14 +1,15 @@
-const { json } = require("express/lib/response");
+// const { json } = require("express/lib/response");
 
+// function agregarProducto(id){
+//     let carrito = []
+//     carrito.push(id)
+//     console.log(carrito);   
+// }
 
+// function guardarStorage(){
+//     localStorage.setItem("carrito", JSON.stringify(carrito))
+// }
 
-function agregarProducto(id){
-    let carrito = []
-    carrito.push(id)
-    console.log(carrito);   
-}
-
-function guardarStorage(){
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-}
-
+let productsArray = [];
+let cartArray = [];
+let totalPrice = 0; 
