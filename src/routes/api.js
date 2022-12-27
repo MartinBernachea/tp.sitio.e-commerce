6560a6252d74eb12b82d5deaeea33437c73f9a10
TobiGondolo <tobiasgondolo@gmail.com>
2022-12-27 20:01:56 -0300

    Add: endpoints del sprint 8

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,8 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 
-router.get ("/users", apiController.usersList);
-router.get("/:id", apiController.user)
+// router.get ("/users", apiController.usersList);
+// router.get("/:id", apiController.user);
+router.get("/products", apiController.productsList);
+router.get("/products/:id", apiController.product);
+router.get("/categories", apiController.categories);
 
 
 module.exports = router;
