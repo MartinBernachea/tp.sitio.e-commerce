33d48cb6b1c50e8e556c33510c5302053cca608f
martin berna <bernacheamartin@outlook.com>
2022-12-28 16:02:26 -0300

    createForm img preview fixed

--- public/js/validationsNewProductForm.js
+++ public/js/validationsNewProductForm.js
@@ -1,8 +1,27 @@
+// IMAGE UPLOAD
+
+function previewBeforeUpload(id) {
+    document.querySelector("#" + id).addEventListener("change", function (e) {
+        if (e.target.files.lenght == 0) {
+            return;
+        }
+        let file = e.target.files[0];
+        let url = URL.createObjectURL(file);
+        document.querySelector("#" + id + "-preview div").innerText = file.name;
+        document.querySelector("#" + id + "-preview img").src = url;
+    })
+}
+
+previewBeforeUpload("cImage1");
+previewBeforeUpload("cImage2");
+previewBeforeUpload("cImage3");
+previewBeforeUpload("cImage4");
+previewBeforeUpload("cImage5");
 
 const submitButton = document.querySelector("#submitButton")
 
 submitButton.addEventListener("click", (event) => {
-    event.preventDefault();
+    // event.preventDefault();
 
     console.log("CLICK")
 
