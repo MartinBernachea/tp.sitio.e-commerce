e9437b6a18521a5866b9dc573451bb088fdb31e3
TobiGondolo <tobiasgondolo@gmail.com>
2023-01-02 23:41:43 -0300

    Add: principio del carrito

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,11 +1,18 @@
-const { json } = require("express/lib/response");
-
-
+const { Json } = require("sequelize/types/utils");
 
 function agregarProducto(id){
-    let carrito = []
-    carrito.push(id)
-    console.log(carrito);   
+    let informacionLocalStorage = localStorage.getItem("carrito");
+    let carrito;
+        if (informacionLocalStorage){
+            carrito = JSON.parse(informacionLocalStorage);
+        }else{
+            carrito = [];
+        };
+    const lugarDelId = carrito.indexOf(id);
+        if(lugarDelId == -1){
+            carrito.push(id);
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+        }
 }
 
 function guardarStorage(){
