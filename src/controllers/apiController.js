9f1f4282861bcd89820b551d7a16e3a0954e18e1
arsitta <sitta93@gmail.com>
2023-01-04 22:42:05 -0300

    FIX: Api Rutas Users

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -3,55 +3,55 @@ const sequelize = require("sequelize");
 
 
 const controller = {
-    // usersList: (req, res) => {
-    //     db.usuario.findAll({attributes: ["id", "nombre", "apellido", "email"]})
-    //         .then (usuarios => {
-    //             return res.status(200).json({
-    //                 count: usuarios.length,
-    //                 users: usuarios,
-    //                 status: 200  
-    //             })
-    //         })
-    // },
-    // user: (req, res) => {
-    //     db.usuario.findByPk(req.params.id, {attributes: ["id", "nombre", "apellido", "email"]})
-    //         .then(usuario => {
-    //             if(usuario !== null){
-    //                 return res.json({
-    //                     data: usuario,
-    //                     status: 200
-                
-    //                 })
-    //             }else{
-    //                 return res.status(404).json({
-    //                     error: "no se encontro el usuario",
-    //                     status: 404
-    //                 })
-    //             }
-    //         })
-    // },
+    usersList: (req, res) => {
+        db.usuario.findAll({ attributes: ["id", "nombre", "apellido", "email"] })
+            .then(usuarios => {
+                return res.status(200).json({
+                    count: usuarios.length,
+                    users: usuarios,
+                    status: 200
+                })
+            })
+    },
+    user: (req, res) => {
+        db.usuario.findByPk(req.params.id, { attributes: ["id", "nombre", "apellido", "email"] })
+            .then(usuario => {
+                if (usuario !== null) {
+                    return res.json({
+                        data: usuario,
+                        status: 200
+
+                    })
+                } else {
+                    return res.status(404).json({
+                        error: "no se encontro el usuario",
+                        status: 404
+                    })
+                }
+            })
+    },
     productsList: async (req, res) => {
         try {
             const database = await db.producto.findAll()
             res.status(200).json({
                 count: database.length,
                 products: database,
-                status: 200  
-                })
-            
-        } catch(error){
+                status: 200
+            })
+
+        } catch (error) {
             res.status(404)
         }
     },
     product: (req, res) => {
         db.producto.findByPk(req.params.id)
             .then(producto => {
-                if(producto !== null){
+                if (producto !== null) {
                     return res.json({
                         data: producto,
-                        status:200
+                        status: 200
                     })
-                }else{
+                } else {
                     return res.status(404).json({
                         error: "no se encontro el producto",
                         status: 404
@@ -64,7 +64,7 @@ const controller = {
             .then(categorias => {
                 return res.status(200).json({
                     data: categorias,
-                    status:200
+                    status: 200
                 })
             })
     }
