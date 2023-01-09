8d83f9b90c40acbe2d474f70f3e20526985fba0b
arsitta <sitta93@gmail.com>
2023-01-09 20:17:06 -0300

    FIX: Estilos form create product

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,6 +1,7 @@
 const db = require("../database/models");
 const sequelize = require("sequelize");
 const { where } = require("sequelize");
+const { agregarCantidadesProductos } = require("../database/utils/generics");
 
 
 const controller = {
@@ -61,33 +62,42 @@ const controller = {
             })
     },
     lastProduct: (req, res) => {
-        db.producto.findAll({include: [{ model: db.genero }, { model: db.marca }, { model: db.categoria }, { model: db.imagen }],   
-                            limit: 1,
-                            order: [["created_at", "DESC"], ["id", "DESC"]]
+        db.producto.findAll({
+            include: [{ model: db.genero }, { model: db.marca }, { model: db.categoria }, { model: db.imagen }],
+            limit: 1,
+            order: [["created_at", "DESC"], ["id", "DESC"]]
         })
-        .then(ultimoProducto => {
-            if (ultimoProducto !== null) {
-                return res.json({
-                    data: ultimoProducto[0],
-                    status: 200
+            .then(ultimoProducto => {
+                if (ultimoProducto !== null) {
+                    return res.json({
+                        data: ultimoProducto[0],
+                        status: 200
 
-                })
-            } else {
-                return res.status(404).json({
-                    error: "no se encontro el producto",
-                    status: 404
-                })
-            }
-        })
-    },
-    categories: (req, res) => {
-        db.categoria.findAll()
-            .then(categorias => {
-                return res.status(200).json({
-                    data: categorias,
-                    status: 200
-                })
+                    })
+                } else {
+                    return res.status(404).json({
+                        error: "no se encontro el producto",
+                        status: 404
+                    })
+                }
             })
+    },
+
+    categories: async (req, res) => {
+        const categoriasSinCantidadProductos = await db.categoria.findAll()
+
+        const cantidadesProductos = (categoriasSinCantidadProductos.length > 0) ?
+            await db.sequelize.query("SELECT categoria_id as 'id', count(*) as'cantidad' FROM producto GROUP BY categoria_id")
+            : []
+
+        const cantidadProductosFixed = cantidadesProductos[0]
+
+        const categoriasConCantidadProductos = agregarCantidadesProductos(categoriasSinCantidadProductos, cantidadProductosFixed)
+
+        res.status(200).json({
+            data: categoriasConCantidadProductos,
+            status: 200
+        })
     }
 }
 
